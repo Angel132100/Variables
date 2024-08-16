a6f2525a3f58682506d6c56347ca9df541d66e1d
Angel Parker <parkeang000@warren.k12.in.us>
2024-08-16 10:18:29 -0400

    initial commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+num_apples = 20
+num_oranges = 15
+print("num of apples:", num_apples)
+print("mum of oranges:", num_oranges)
+num_oranges = 0
+print("num of apples:", num_apples)
+print("mum of oranges:", num_oranges)
+
+
